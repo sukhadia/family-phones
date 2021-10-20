2417676b0d6cc6e2854f2986afafb75332922c7e
Neel Sukhadia <nsukhadia@callpotential.com>
2021-10-19 22:23:42 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="item" v-for="(person, index) of people" :key="index">
+        <span>{{ person.name }}</span>
+        <span>{{ person.phone }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      people: [
+        {
+          name: "Neel",
+          phone: "888-111-2222",
+        },
+        {
+          name: "Abhijeet",
+          phone: "888-222-3333",
+        },
+        {
+          name: "Ruchir",
+          phone: "888-333-4444",
+        },
+        {
+          name: "Falguni",
+          phone: "888-444-5555",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body,
+html {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+.container {
+  list-style: none;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+}
+.item {
+  width: 100%;
+  border: 5px solid white;
+  padding: 0.75rem;
+  margin: 1rem 0;
+  background: orangered;
+  text-align: left;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 700;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 1rem;
+  box-sizing: border-box;
+  max-width: 60%;
+}
+</style>
